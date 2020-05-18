c5d5f10bed4b608fd0afbcf55898cf6b104592ba
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-18 22:20:27 +0100

    Creat class team constructor

---
+++ spec/team_spec.rb
@@ -0,0 +1,10 @@
+require('minitest/autorun')
+require('minitest/reporters')
+require_relative('../team')
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class TestTeam < MiniTest::Test
+
+
+end
